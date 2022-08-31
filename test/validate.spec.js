ed16c429709b4725e84b6ba91a45a4ca6c067573
day all <danielafuentesdev@gmail.com>
2022-08-31 10:36:53 -0500

    test validation of links

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,8 +1,9 @@
 const verifyLinks = require('../src/validate.js')
-jest.mock('node-fetch', () => jest.fn())
+const fetch = require('node-fetch')
+jest.mock('node-fetch')
 
 describe('verifyLinks must give 200 status code for valid links', () => {
-  it("status: 200 - message: 'Ok'", () => {
+  it("status: 200 - message: 'ok'", () => {
     const recieveObject = [
       {
         href: 'https://docs.npmjs.com/files/package.json',
@@ -16,10 +17,11 @@ describe('verifyLinks must give 200 status code for valid links', () => {
         text: 'package.json - Documentación oficial (en inglés)',
         file: 'C:/Users/cosmo/Documents/Laboratoria_proyects/your-md-links/folder-tests/folderB/filemd2.md',
         status: 200,
-        message: 'Ok'
+        message: 'ok'
       }
     ]
-    return verifyLinks(recieveObject)
+    fetch.mockResolvedValueOnce({ status: 200, ok: true })
+    verifyLinks(recieveObject)
       .then((result) => {
         expect(result).toEqual(resultObject)
       })
